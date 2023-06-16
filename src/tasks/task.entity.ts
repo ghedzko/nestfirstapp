3d2008cdb23cd2dbb98803e4ec9076f08fa1727a
Guillermo Hedzko <ghedzko@gmail.com>
2023-06-16 11:13:13 -0300

    configure task entity on typeorm

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,11 +1,21 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
 export enum TaskStatus {
   PENDING = 'PENDING',
   IN_PROGRESS = 'IN_PROGRESS',
   DONE = 'DONE',
 }
+@Entity({ name: 'tasks' })
 export class Task {
+  @PrimaryGeneratedColumn()
   id: string;
+
+  @Column()
   title: string;
+
+  @Column({ nullable: true })
   description: string;
+
+  @Column({ type: 'varchar', default: TaskStatus.PENDING })
   status: TaskStatus;
 }
