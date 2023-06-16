ff5eaa2e8919641c8c3f8f732d1a72d8291dbf1c
Guillermo Hedzko <ghedzko@gmail.com>
2023-06-16 14:23:26 -0300

    log how many time did request took to be completed

--- src/tasks/interceptors/logging.interceptor.ts
+++ src/tasks/interceptors/logging.interceptor.ts
@@ -12,20 +12,26 @@ export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const httpContext = context.switchToHttp();
     const request = httpContext.getRequest();
+    let inicioTransaccion: number;
 
     if (request.method === 'PATCH') {
-      console.log('Inicio de la petición PATCH:', new Date());
+      inicioTransaccion = Date.now();
+      console.log('Inicio de la petición PATCH:', new Date(inicioTransaccion));
       console.log('Body:', request.body);
     }
 
-    return next
-      .handle()
-      .pipe(
-        tap(
-          () =>
-            request.method === 'PATCH' &&
-            console.log('Fin de la petición PATCH:', new Date()),
-        ),
-      );
+    return next.handle().pipe(
+      tap(() => {
+        if (request.method === 'PATCH') {
+          const finTransaccion = Date.now();
+          console.log('Fin de la petición PATCH:', new Date(finTransaccion));
+          console.log(
+            'Duración de la transacción:',
+            finTransaccion - inicioTransaccion,
+            'ms',
+          );
+        }
+      }),
+    );
   }
 }
