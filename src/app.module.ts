5ab721ce80ab955e15dfdb37b2a7630b6f367e7c
Guillermo Hedzko <ghedzko@gmail.com>
2023-06-16 09:26:01 -0300

    connect to mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'Tasks',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TasksModule,
+  ],
   controllers: [],
   providers: [],
 })
