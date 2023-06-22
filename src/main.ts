72998c6cfd3f468b9bda212be66b10b2912ce986
Guillermo Hedzko <ghedzko@gmail.com>
2023-06-22 17:25:02 -0300

    feat: add RabbitMQ connection service

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,41 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  const microserviceOptions: MicroserviceOptions = {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://guest:guest@127.0.0.1:5672'], // La URL de tu servidor RabbitMQ
+      queue: 'tasks_queue', // El nombre de tu cola en RabbitMQ
+      queueOptions: {
+        durable: false,
+      },
+    },
+  };
+
+  app.connectMicroservice(microserviceOptions);
 
   const options = new DocumentBuilder()
     .setTitle('Tasks API')
-    .setDescription('The task API description')
+    .setDescription('The tasks API description')
     .setVersion('1.0')
     .addTag('tasks')
-
     .addBearerAuth()
     .build();
+
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
+
+  await app.startAllMicroservices();
   await app.listen(3000, '127.0.0.1');
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
